2969ff7578dd782c9463a576e69e60b4369ee9ac
naimeds10 <giesdb@gmail.com>
2021-08-12 01:00:15 +0200

    introduced bgg

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -2,6 +2,10 @@ import Discord from 'discord.js'
 import fs from 'fs'
 
 import { prefix } from '../../config.json'
+import {
+    CollectorPlayerLeftError,
+    CollectorPlayerPassedInput,
+} from '../game/Errors'
 import { ServerManager } from '../managers/ServerManager'
 
 const commandFiles = fs
@@ -22,6 +26,7 @@ export class Client extends Discord.Client {
         this.commands = new Discord.Collection()
         this.games = new Discord.Collection()
         this.serverManager = new ServerManager()
+        throw new CollectorPlayerLeftError('Je moeder kk sentry')
 
         // Set commands from /src/commands
         for (const file of commandFiles) {
