ffa2c70ab28c9835bd87004d6ff722a3d41455d7
naimeds10 <giesdb@gmail.com>
2021-08-12 00:52:11 +0200

    introduced bug

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -2,7 +2,7 @@ import Discord from 'discord.js'
 import fs from 'fs'
 
 import { prefix } from '../../config.json'
-import { CollectorPlayerLeftError } from '../game/Errors'
+import { CollectorPlayerLeftError, GameEndedError } from '../game/Errors'
 import { ServerManager } from '../managers/ServerManager'
 
 const commandFiles = fs
@@ -23,7 +23,7 @@ export class Client extends Discord.Client {
         this.commands = new Discord.Collection()
         this.games = new Discord.Collection()
         this.serverManager = new ServerManager()
-        throw new CollectorPlayerLeftError('BUggy shit jo')
+        throw new GameEndedError('Err')
 
         // Set commands from /src/commands
         for (const file of commandFiles) {
